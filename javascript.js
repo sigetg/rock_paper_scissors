c6480701a22388eaec85ae9673aadd562c31dcf9
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-12 17:39:55 -0400

    Convert from console game to HTML button game
    Added a title, buttons, and an output to the window, and made them
    responsive using event listeners. Made some edits to other functions to
    make it work.

--- javascript.js
+++ javascript.js
@@ -6,26 +6,23 @@ function getComputerChoice() {
 }
 
 //returns game result of win, loose, tie, or again, which means the user entered an incorrect value
-function playRound(computerSelection, playerSelection) {
+function getGameResult(computerSelection, playerSelection) {
     let gameResult;
     switch(computerSelection) {
         case 'rock':
             gameResult = 
             playerSelection === 'rock' ? "tie" :
-            playerSelection === 'scissors' ? "loose" : 
-            playerSelection === 'paper' ? "win" : "again";
+            playerSelection === 'scissors' ? "loose" : "win";
             break;
         case 'paper':
             gameResult = 
             playerSelection === 'rock' ? "loose" :
-            playerSelection === 'scissors' ? "win" : 
-            playerSelection === 'paper' ? "tie" : "again";
+            playerSelection === 'scissors' ? "win" : "tie";
             break;
         case 'scissors':
             gameResult = 
             playerSelection === 'rock' ? "win" :
-            playerSelection === 'scissors' ? "tie" : 
-            playerSelection === 'paper' ? "loose" : "again";
+            playerSelection === 'scissors' ? "tie" : "loose";
             break;
         default:
             gameResult = "again";
@@ -35,30 +32,31 @@ function playRound(computerSelection, playerSelection) {
 
 //shows different message in console depending on gameResult
 function showGameResult(gameResult, playerSelection, computerSelection) {
-    console.log(
-        gameResult === 'win' || gameResult === 'loose' ? 
+    const playerChoice = document.querySelector(".playerChoice");
+    const computerChoice = document.querySelector(".computerChoice");
+    const gameOutcomeMessage = document.querySelector(".gameOutcomeMessage");
+    playerChoice.textContent = `player: ${playerSelection}`;
+    computerChoice.textContent = `computer: ${computerSelection}`;
+    gameOutcomeMessage.textContent = gameResult === 'win' || gameResult === 'loose' ? 
         `You ${gameResult}! ${playerSelection} ${gameResult === "win" ? "beats" : "looses to"} ${computerSelection}.` : 
-        gameResult === 'tie' ? "It's a tie!" : "Please enter either rock, paper, or scissors."
-    );
+        gameResult === 'tie' ? "It's a tie!" : "Please enter either rock, paper, or scissors.";
 }
 
-//main function. runs playRound five times unless user decides not to play again. 
-function game() {
-    let gamesWon = 0;
-    for (let i=0; i < 5; i++) {
-        const playerSelection = prompt("Please enter rock, paper, or scissors.").toLowerCase();
-        const computerSelection = getComputerChoice();
-        const gameResult = playRound(computerSelection, playerSelection);
-        console.log(`player: ${playerSelection}`);
-        console.log(`computer: ${computerSelection}`);
-        showGameResult(gameResult, playerSelection, computerSelection)
-        const anotherGame = confirm("Play Again?");
-        if (anotherGame === true) {
-            continue;
-        } else {
-            break;
-        }
-    }
+function playRound(buttonClass) {
+    const playerSelection = buttonClass;
+    const computerSelection = getComputerChoice();
+    const gameResult = getGameResult(computerSelection, playerSelection);
+    showGameResult(gameResult, buttonClass, computerSelection)
 }
 
-game();
+//main function. Listens for button presses.
+function main() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playRound(button.className);
+        });
+    })
+}
+
+main();
