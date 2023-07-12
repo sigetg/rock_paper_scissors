1e81377239bb5663eedd36127cd12d8e948677e3
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-12 10:04:47 -0400

    Create and test playRound function
    Added some code to allow a round to be played through a prompt and
    the console through the playRound function, which uses a switch to
    handle conditional logic for the game.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,39 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(3*Math.random())];
 }
 
-console.log(getComputerChoice())
+
+function playRound(computerSelection, playerSelection) {
+    let gameResult;
+    switch(computerSelection) {
+        case 'rock':
+            gameResult = 
+            playerSelection === 'rock' ? "It's a tie!" :
+            playerSelection === 'scissors' ? "You loose! Rock beats scissors." : 
+            playerSelection === 'paper' ? "You win! Paper beats rock." : "Please enter either rock, paper, or scissors.";
+            break;
+        case 'paper':
+            gameResult = 
+            playerSelection === 'rock' ? "You loose! Paper beats rock." :
+            playerSelection === 'scissors' ? "You win! Scissors beats paper" : 
+            playerSelection === 'paper' ? "It's a tie!" : "Please enter either rock, paper, or scissors.";
+            break;
+        case 'scissors':
+            gameResult = 
+            playerSelection === 'rock' ? "It's a tie!" :
+            playerSelection === 'scissors' ? "It's a tie!" : 
+            playerSelection === 'paper' ? "You win! Paper beats rock" : "Please enter either rock, paper, or scissors.";
+            break;
+        default:
+            gameResult = "Please enter either rock, paper, or scissors.";
+    }
+    return gameResult;
+}
+
+const playerSelection = prompt("Please enter rock, paper, or scissors.");
+console.log(`player: ${playerSelection}`)
+const computerSelection = getComputerChoice()
+console.log(`computer: ${computerSelection}`)
+const gameResult = playRound(computerSelection, playerSelection.toLowerCase())
+console.log(gameResult)
